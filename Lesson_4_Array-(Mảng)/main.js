6d549b81578fcc5a431123e12f2a82964b88b2ba
TrungTien2004_@UTH <118372166+Nguyen-Trung-Tien@users.noreply.github.com>
2025-05-15 15:29:57 +0700

    HTML DOM

--- Lesson_4_Array-(Mảng)/main.js
+++ Lesson_4_Array-(Mảng)/main.js
@@ -1,15 +1,6 @@
 // Cách tạo
-
 var array1 = [1, 2, 3, 4, 5]; //cách 1
-var languages = new Array(
-    "Java",
-    "JS",
-    "PHP",
-    "C#",
-    "C++",
-    "C",
-    "Python"
-); //cách 2
+var languages = new Array("Java", "JS", "PHP", "C#", "C++", "C", "Python"); //cách 2
 
 console.log(array1);
 console.log(languages);
@@ -28,7 +19,7 @@ console.log(languages.pop());
 // Push
 console.log(languages.push("Java", "JS", "PHP", "{}"));
 
-// Shift    
+// Shift
 console.log(languages.shift());
 
 // Unshift
@@ -46,95 +37,92 @@ console.log(languages1.concat(languages2));
 // Slicing
 console.log(languages1.slice(1, 3));
 
-// ọbject
+// object
 
 var object = {
-    name: "A",
-    age: 18,
-    address: "VN"
+  name: "A",
+  age: 18,
+  address: "VN",
 };
 console.log(object);
 
-
-console.log("===================");
+console.log("=================== object");
 //forEach()
 
 var courses = [
-    {
-        id: 1,
-        name: "Java",
-        coin: 100,
-    },
-    {
-        id: 2,
-        name: "JS",
-        coin: 0,
-    },
-    {
-        id: 3,
-        name: "PHP",
-        coin: 300,
-    },
+  {
+    id: 1,
+    name: "Java",
+    coin: 100,
+  },
+  {
+    id: 2,
+    name: "JS",
+    coin: 0,
+  },
+  {
+    id: 3,
+    name: "PHP",
+    coin: 300,
+  },
 
-    {
-        id: 4,
-        name: "C",
-        coin: 0,
-    },
-    {
-        id: 5,
-        name: "C#",
-        coin: 200,
-    },
+  {
+    id: 4,
+    name: "C",
+    coin: 0,
+  },
+  {
+    id: 5,
+    name: "C#",
+    coin: 200,
+  },
 
-    {
-        id: 6,
-        name: "C#",
-        coin: 300,
-    }
-]
+  {
+    id: 6,
+    name: "C#",
+    coin: 300,
+  },
+];
 
 // forEach()
 courses.forEach(function (courses, index) {
-    console.log(index, courses);
-
+  console.log(index, courses);
 });
 
 // every()
 
-var isfree = courses.every(function (courses, index) {
-    return courses.coin === 0;
+var isFree = courses.every(function (courses, index) {
+  return courses.coin === 0;
 });
 
-console.log(isfree);
+console.log(isFree);
 
 // some()
-var isfree2 = courses.some(function (courses, index) {
-    return courses.coin === 0;
+var isFree2 = courses.some(function (courses, index) {
+  return courses.coin === 0;
 });
-console.log(isfree2);
+console.log(isFree2);
 // find()
 var course = courses.find(function (courses, index) {
-    return courses.name === "JS";
+  return courses.name === "JS";
 });
 console.log(course);
 
 // filter
 var listCourse = courses.filter(function (courses, index) {
-    return courses.name === "C#";
+  return courses.name === "C#";
 });
 console.log(listCourse);
-
 
 console.log("=================== Map()");
 // map()
 var courseHandler = function (courses, index) {
-    return {
-        id: courses.id,
-        name: 'khóa học' + courses.name,
-        coin: 'giá' + courses.coin,
-    };
-}
+  return {
+    id: courses.id,
+    name: "khóa học" + courses.name,
+    coin: "giá" + courses.coin,
+  };
+};
 
 var newCourse = courses.map(courseHandler);
 console.log(newCourse);
--- Lesson_6-Loop-(vòng lập)/main.js
+++ Lesson_6-Loop-(vòng lập)/main.js
@@ -1,120 +1,102 @@
 // for
 
-console.log('============== for')
-var myArray = [
-    'Javascript',
-    'PHP',
-    'Java',
-    'C'
-];
+console.log("============== for");
+var myArray = ["Javascript", "PHP", "Java", "C"];
 
 var arraylength = myArray.length;
 for (var i = 0; i < arraylength; i++) {
-    console.log(myArray[i]);
+  console.log(myArray[i]);
 }
 
-console.log('=============== for/in')
+console.log("=============== for/in");
 
 // for/int
-var myLanguages = [
-    'Java',
-    'Javascript',
-    'Ruby',
-    'C'
-]
+var myLanguages = ["Java", "Javascript", "Ruby", "C"];
 
 for (var key in myLanguages) {
-    console.log(myLanguages[key])
+  console.log(myLanguages[key]);
 }
 
-console.log('=============== for/in')
+console.log("=============== for/in");
 
 // dùng for/in tách thành các ký tự
 
-var myString = 'JavaScript';
+var myString = "JavaScript";
 
 for (var key in myString) {
-    console.log(myString[key])
+  console.log(myString[key]);
 }
 
-console.log('============= for/of')
+console.log("============= for/of");
 // for/of
 
-var myString2 = 'Java'
+var myString2 = "Java";
 
 for (var value of myString2) {
-    console.log(value)
+  console.log(value);
 }
 
 // lấy giá trị value của object
 var myInfo = {
-    name: 'Nguyen van a',
-    Age: 20,
-    email: 'Ac@example.com',
+  name: "Nguyen van a",
+  Age: 20,
+  email: "Ac@example.com",
 };
 
 for (var value of Object.values(myInfo)) {
-    console.log(value);
+  console.log(value);
 }
 
-
-console.log('============== While')
+console.log("============== While");
 // while
 
-
 while (i <= 10) {
-    i++;
-    console.log(i)
-};
+  i++;
+  console.log(i);
+}
 
-console.log('============== Do/While')
+console.log("============== Do/While");
 // do/while
 var i = 0; // kiểm tra nạp tiền
 var isSuccess = false;
 do {
-    i++;
-    console.log('nap tien lan' + i);
-    if (true) {
-        isSuccess = true
-    }
+  i++;
+  console.log("nap tien lan" + i);
+  if (true) {
+    isSuccess = true;
+  }
 } while (!isSuccess && i < 3);
 
-
-console.log('============== Break')
+console.log("============== Break");
 // break: thoát khỏi vòng lập
 
 for (var i = 0; i < 10; i++) {
-    console.log(i);
-    if (i >= 5) {
-        break;
-    }
+  console.log(i);
+  if (i >= 5) {
+    break;
+  }
 }
 
-
-
-console.log('============== Continue')
+console.log("============== Continue");
 // continue: bỏ qua điều kiện
 for (var i = 0; i < 10; i++) {
-    if (i % 2 !== 0) {
-        continue;
-    }
+  if (i % 2 !== 0) {
+    continue;
+  }
 
-    console.log(i);
+  console.log(i);
 }
 
-
-console.log('============== Nested Loop')
-// vòng lặp lòng nhau
+console.log("============== Nested Loop");
+// vòng lặp lòng nhau -Nested Loop
 var myArray = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-
+  [1, 2],
+  [3, 4],
+  [5, 6],
 ];
 
 for (var i = 0; i < myArray.length; i++) {
-
-    for (var j = 0; j < myArray[i].length; j++) {
-        console.log(myArray[i][j]);
-    }
+  for (var j = 0; j < myArray[i].length; j++) {
+    console.log(myArray[i][j]);
+  }
 }
